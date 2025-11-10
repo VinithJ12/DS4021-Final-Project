dc6c902331ee16d9a95c4213d881032a45f8227f
Iliana Vasslides <ilianamvass@gmail.com>
2025-11-10 18:18:09 -0500

    Uploading test set

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DS4021-Final-Project
-A repository for our DS 4021 Final Project, predicting fetal health status using different features and 
+A repository for our DS 4021 Final Project: predicting fetal health status using Binary Model Classification.  
---
+++ data_cleaning.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+
+# loading csv file into dataframe
+df = pd.read_csv('fetal_health.csv')
+
+# only keeping relevant columns 
+col_keeping = ['baseline value', 'accelerations', 'fetal_movement', 'uterine_contractions', 'light_decelerations', 'severe_decelerations',
+       'prolongued_decelerations', 'abnormal_short_term_variability', 'mean_value_of_short_term_variability',
+       'percentage_of_time_with_abnormal_long_term_variability', 'mean_value_of_long_term_variability', 'fetal_health']
+df = df[col_keeping]
+print(df.columns) 
+
+# train test splitting the data 
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+
+# saving training set to csv
+train_df.to_csv("./Data/train_set.csv", index=False)
+
+# saving test set to csv
+test_df.to_csv("./Data/test_set.csv", index=False)
+
+print("Training and test sets saved as csv files.") 
